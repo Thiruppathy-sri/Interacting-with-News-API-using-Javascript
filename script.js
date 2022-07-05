76f01fdc65db1b917537d5357c2c863a3518125b
Thiruppathy Sri <thiruppathy.sri@gmail.com>
2022-07-05 14:19:28 +0530

    code changes with async await

--- script.js
+++ script.js
@@ -2,18 +2,14 @@ const rowEl = document.querySelector(".row");
 const newsdataFeed = document.querySelector(".newsdata-feed");
 const selectEl = document.querySelector("#category");
 
-
-const renderData = function(data) {
+const renderData = function (data) {
   let htmlEle = "";
-  if (data.data) 
-    console.log(data.data);
-    data.data.forEach((newsData) => {
-      htmlEle += `
+  if (data.data) console.log(data.data);
+  data.data.forEach((newsData) => {
+    htmlEle += `
       <div class="col-4 mb-4">
       <div class="card">
-      <img class="card-img-top" src="${
-        newsData.imageUrl
-      }" alt="Card image cap">
+      <img class="card-img-top" src="${newsData.imageUrl}" alt="Card image cap">
       
       <div class="card-body">
        <div class="d-flex justify-content-between date-time">
@@ -27,26 +23,27 @@ const renderData = function(data) {
        </div>
       </div>
       `;
-    });
-     newsdataFeed.innerHTML = htmlEle;
-}
+  });
+  newsdataFeed.innerHTML = htmlEle;
+};
 
 const newsData = async function (catName) {
   try {
-    const res =  await fetch(`https://inshorts.deta.dev/news?category=${catName}`);
-    if(!res.ok) {
+    const res = await fetch(
+      `https://inshorts.deta.dev/news?category=${catName}`
+    );
+    if (!res.ok) {
       throw new Error(`Something went wrong... Try Again${res.status}`);
-     }
-     const data = await res.json();
-     renderData(data)
-  }  catch(err) {
+    }
+    const data = await res.json();
+    renderData(data);
+  } catch (err) {
     htmlEle = `<h3 class="text-center">Something went wrong... Try Again ${err.message}</h3>`;
     newsdataFeed.innerHTML = htmlEle;
     console.log(`${err.message}`);
   }
-  newsdataFeed.style.opacity = '1';
-}
-
+  newsdataFeed.style.opacity = "1";
+};
 
 selectEl.addEventListener("change", function () {
   newsData(selectEl.value);
